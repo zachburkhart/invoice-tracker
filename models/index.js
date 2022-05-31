5629ca2b1d5982adc8841eb7a03da30742fb7a6c
Josh Klein <kleinjjosh74@gmail.com>
2022-05-31 19:42:24 -0400

    added model structure

--- models/index.js
+++ models/index.js
@@ -1,5 +1,12 @@
 const User = require('./User');
 const Expense = require('./Expense');
 
+User.hasMany(Expense, {
+    foreignKey: 'user_id'
+});
+
+Expense.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 module.exports = { User, Expense };
