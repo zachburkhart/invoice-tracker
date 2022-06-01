6c43e37949e617c895150b688bfb9bd088da03f1
mohamed behnas <behna@DESKTOP-T1K4JBI.(none)>
2022-05-31 20:46:41 -0400

    update dashboar-routes.js/home-route.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,54 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
+//const sequelize = require('../config/connection');
 const { User, Expense } = require('../models');
 const authorization = require('../utils/auth');
+
+
+
+router.get('/', authorization, (req,res)=>{
+    Expense.findAll({
+        where: {
+            user_id: req.session.user_id
+          },
+          attributes: [
+              'id',
+              'title',
+              'description',
+              'expense_value',
+              'date_due',
+          ],    
+
+        })
+        .then(dbExpenseData => {
+            const expenses = dbExpenseData.map(expense => expense.get({ plain: true }));
+            res.render('dashboard', { expenses, loggedIn: true });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      });
+
+      router.get('/edit/:id', authorization, (req, res) => {
+        Expense.findByPk(req.params.id, {
+          attributes: [
+            'id',
+            'title',
+            'description',
+            'expense_value',
+            'date_due',
+        ],    
+    })
+        .then(dbExpenseData => {
+            const expenses = dbExpenseData.map(expense => expense.get({ plain: true }));
+            res.render('edit-expense', { expenses, loggedIn: true });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      });
+      
+
 
 module.exports = router;
