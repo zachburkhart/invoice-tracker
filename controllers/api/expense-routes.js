2bbfc9bfdbc2a8c15afb4812f95b99580f8a9a25
Zach Burkhart <zachburkhart216@gmail.com>
2022-06-01 20:17:41 -0400

    udpated api routes

--- controllers/api/expense-routes.js
+++ controllers/api/expense-routes.js
@@ -2,6 +2,21 @@ const router = require('express').Router();
 const { User, Expense } = require('../models');
 const authorization = require('../utils/auth');
 
+router.get('/', (req,res) => {
+    Expense.findAll({
+        where:{
+            id:req.session.user_id
+        },
+        attributes:[
+            "id",
+            "title",
+            "description",
+            "expense_value",
+            "date_due",
+        ],
+        order:[["date_due",'DESC']]
+    })
+})
 
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,52 @@
 const router = require('express').Router();
-const { User, Expense } = require('../models');
+const { User } = require('../models');
 
+router.post('/', (req,res) => {
+    User.create({
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
 
+router.post('/login', (req,res) => {
+    User.findOne({
+        where:{
+            email:req.body.email,
+        }
+    }) .then(dbUserData => {
+        if(!dbUserData){
+            res.status(400).json({
+                message:"No user with that email address"
+            }); return;
+        } 
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword) {
+            res.status(400).json({
+                message:"Incorrect password"
+            }); return;
+        }
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+            res.json({
+                user:dbUserData,message:"You're not logged in"
+            });
+        })
+    }) 
+})
 
 module.exports = router;
